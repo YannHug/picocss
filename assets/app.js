86ae8358732c14c5c214a88585d20e41b432a25c
Yann HUG <yannhweb@gmail.com>
2022-04-02 16:20:24 +0200

    📦 NEW: dark mode

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,14 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+let darkButton = document.querySelector('#dark');
+let theme = document.querySelector('#theme');
+
+darkButton.addEventListener('click', () => {
+    if (theme.getAttribute('data-theme') === 'light') {
+        theme.setAttribute('data-theme', 'dark');
+    } else {
+        theme.setAttribute('data-theme', 'light');
+    }
+})
